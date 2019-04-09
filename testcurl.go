ee8afcf5dd57698d273ca468007a541acc8c245a
kraser <s.kraval@e2e4online.ru>
2019-04-09 17:47:53 +0700

    Request Cookies

--- testcurl.go
+++ testcurl.go
@@ -21,7 +21,7 @@ func main() {
 	options.Url = url
 	options.CookieFile = "/home/robot/e2.cookie"
 	client := curl.InitCurl(options)
-	fmt.Println(client)
+	//fmt.Println(client)
 	result := client.DoRequest(url)
 	writeHtmlToFile(result)
 }
